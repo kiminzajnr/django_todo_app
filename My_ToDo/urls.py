a8fd551ec6a03a334d36c7a9e4c7832a1643f894
kiminzajnr <kiminzaeric@gmail.com>
2024-04-16 23:48:55 +0300

    add url to main view

--- My_ToDo/urls.py
+++ My_ToDo/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("ToDo.urls")),
+    path("index/", include("ToDo.urls")),
     path('admin/', admin.site.urls),
 ]
