f0200cad0233ba71b297235db585a14f376b5c96
kiminzajnr <kiminzaeric@gmail.com>
2024-04-16 23:51:49 +0300

    update index view

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 def index(request):
-    return render(request, "ToDo/index.html")
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('index')
+    return render(request, "ToDo/index.html", {"tasks": tasks, "form": form})
