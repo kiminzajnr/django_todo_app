f28dc14a6619e77bdfb989b38152be5f8ad2bbdf
kiminzajnr <kiminzaeric@gmail.com>
2024-04-16 23:50:05 +0300

    create Task model

--- ToDo/models.py
+++ ToDo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    todo_task = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.todo_task
+
