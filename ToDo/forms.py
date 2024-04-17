deffaa228610415b5ea66d79f50ca89fff73ccd4
kiminzajnr <kiminzaeric@gmail.com>
2024-04-17 22:43:58 +0300

    add attr for styling

--- ToDo/forms.py
+++ ToDo/forms.py
@@ -6,3 +6,6 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["todo_task"]
+        widgets = {
+            'todo_task': forms.TextInput(attrs={'class': 'control'}),
+        }
