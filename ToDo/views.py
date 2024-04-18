32ea21a5126527b72316f1d0f260d030e0e77fc6
kiminzajnr <kiminzaeric@gmail.com>
2024-04-18 23:12:56 +0300

    fix missing class param

--- ToDo/views.py
+++ ToDo/views.py
@@ -13,7 +13,7 @@ def index(request):
     return render(request, "ToDo/index.html", {"tasks": tasks, "form": form})
 
 def delete_task(request, task_id):
-    task = get_object_or_404(id=task_id)
+    task = get_object_or_404(Task, id=task_id)
     if request.method == "POST":
         task.delete()
         return redirect('ToDo:index')
