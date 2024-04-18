6a655c19b12330f2c33e7be47c5a2e1778dc4d81
kiminzajnr <kiminzaeric@gmail.com>
2024-04-19 00:27:41 +0300

    add my ip to allowed hosts

--- My_ToDo/settings.py
+++ My_ToDo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '192.168.0.102']
 
 
 # Application definition
