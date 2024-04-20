b180c906188bfed0a43a8f12c569e42534b54c2e
kiminzajnr <kiminzaeric@gmail.com>
2024-04-20 15:17:44 +0300

    url for edit

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -6,5 +6,6 @@ from .import views
 app_name = "ToDo"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("edit/<int:task_id>", views.edit_task, name="edit_task"),
     path("delete/<int:task_id>", views.delete_task, name="delete_task"),
 ]
